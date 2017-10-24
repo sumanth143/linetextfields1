512cb57d383b42c67746f4ea201f3256b691b4ce
student <student@achiever-31.local>
2017-10-24 16:36:07 +0530

    Initial Commit

---
+++ linetf/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  linetf
+//
+//  Created by student on 10/24/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ linetf/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  linetf
+//
+//  Created by student on 10/24/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ACFloatingTextField.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
